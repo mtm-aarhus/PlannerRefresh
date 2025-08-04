99ec8d477d01fa98795130565fc69752662440a6
Laura-Salmonsen <balas@aarhus.dk>
2025-08-04 08:31:55 +0200

    Update process.py

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -57,8 +57,8 @@ def download_planner(downloads_folder, planner_url, final_file_path, orchestrato
     options.add_argument("--user-data-dir=" + os.path.join(os.getenv("LOCALAPPDATA"), "Microsoft", "Edge", "User Data"))
     options.add_argument("--start-maximized")
     options.add_argument("--disable-extensions")
-    # options.add_argument("--profile-directory=Default")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--profile-directory=Default")
+    # options.add_argument("--remote-debugging-port=9222")
 
     prefs = {
         "download.default_directory": downloads_folder,
